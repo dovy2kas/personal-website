4404716af938294973a41b60c421c78a656d4e16
dovy2kas <dovixyder@gmail.com>
2024-01-23 20:05:41 +0200

    Removed blog

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -36,9 +36,6 @@ const Navigation = () => {
                         <li>
                             <a href="/#contact" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
                         </li>
-                        <li>
-                            <a href="/blog" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Cybersec ventures</a>
-                        </li>
                     </ul>
                 </div>
             </div>
